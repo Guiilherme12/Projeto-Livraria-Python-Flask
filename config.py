907a1ba2985ef72a74a5055ed085936a72f0713a
Guiilherme12 <joaoguilhermesbueno12@gmail.com>
2026-03-14 14:39:53 -0300

    :package:Commit Inicial

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = "|pzG[#=pD@dP^e$gpbuOUt!%^X*(zC"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///bookstrote.db" #Variavel de conexao com o banco de dados
+    SQLALCEMY_TRACK_MODIFICATIONS = False
